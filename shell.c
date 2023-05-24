e3b4e6fdb09bcc74f6059b4a760c665bf5f311b4
enocklennox <lennoxkipngeno@gmail.com>
2023-05-24 17:24:19 +0300

    correction

--- shell.c
+++ shell.c
@@ -7,51 +7,37 @@
  *     *
  *      * Return: 0 on success, otherwise 1
  *       */
-int main(int ac, char **argv)
+
+int main(int ac, char **av)
 {
-	char *user_input, **receive_argv;
-	ssize_t fd_check;
-	int i, execution_status, cd_result;
-	data_shell shell_data;
-
-	shell_data._environ = environ;
-	(void)ac;
-	(void)argv;
-
-	while (1 == 1)
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+	if (ac == 2)
 	{
-	user_input = NULL;
-	fd_check = 0;
-	user_input = prompt_read(&fd_check);
-	receive_argv = tokenization(user_input, receive_argv, fd_check);													if (receive_argv[0] == NULL)
-	continue;
-	if (_strcmp(receive_argv[0], "env") == 0)
-	{
-	free(user_input);
-	_env(&shell_data);
-	continue;
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
 	}
-	if (_strcmp(receive_argv[0], "exit") == 0)
-	handle_exit(user_input, receive_argv);
-	if (_strcmp(receive_argv[0], "cd") == 0)
-	{
-		cd_result = cd_command(receive_argv);
-		if (cd_result == -1)
-			print_error(receive_argv, "cd failed\n");
-		continue;
-	}
-	i = handle_env_commands(receive_argv);
-	if (i != 0)
-	{
-		free(user_input);
-		free_array(receive_argv);
-		continue;
-	}
-	execution_status = execute_command(receive_argv);
-	if (execution_status == 1)
-		print_error(receive_argv, "Command not found\n");
-	free(user_input);
-	free_array(receive_argv);
-	}
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
